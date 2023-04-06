e6a4925be1ccb85c22a319902195c1a3630d8df1
vladhoang <ndsdarkwalker@gmail.com>
2023-04-06 09:36:42 +0700

    add Kubenetes files

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=sapassword
-export POSTGRES_HOST=phuctnh1-db.cv2cbymv28ss.us-east-2.rds.amazonaws.com
+export POSTGRES_HOST=database-master.cn4rmdner9lh.us-west-2.rds.amazonaws.com
 export POSTGRES_DB=database_master
 export AWS_BUCKET=arn:aws:s3:::pj3bucket
-export AWS_REGION=us-east-2
+export AWS_REGION=us-west-2
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
